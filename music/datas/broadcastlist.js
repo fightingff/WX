d56e3a1229e741ded11c69ce622865838a8a0b05
ff <2575015026@qq.com>
2022-11-29 21:51:51 +0800

    first music audios that can't actually play well

---
+++ music/datas/broadcastlist.js
@@ -0,0 +1,29 @@
+var json=[
+  {
+    title: "个性电台",
+    poster: "../../images/blist1.jfif"
+  },
+  {
+    title: "带感英文",
+    poster: "../../images/blist2.jpg"
+  },
+  {
+    title: "抖音神曲",
+    poster: "../../images/blist3.png"
+  },
+  {
+    title: "经典流传",
+    poster: "../../images/blist4.jpg"
+  },
+  {
+    title: "动人情歌",
+    poster: "../../images/blist5.jpg"
+  },
+  {
+    title: "劲爆金曲",
+    poster: "../../images/blist6.jfif"
+  },
+]
+module.exports={
+  list: json
+}
